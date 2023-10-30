6fe5410b0cd2e47499f262494df92c02d6808e36
PerfectionistAF <ihabsohaila@yahoo.com>
2023-10-30 15:03:06 +0200

    Assessment Project

---
+++ admin/includes/showUserDetails.php
@@ -0,0 +1,24 @@
+<?php
+if($status){
+	#show car details
+	try{
+		$sql = "SELECT * FROM `users_table` WHERE id = ?";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute([$id]);
+		$row = $stmt->fetch();
+		$fullname = $row["fullname"];
+		$username = $row["username"];
+		$email = $row["email"];
+		if(isset($_POST["active"])){
+		  $activeBox = "checked";
+		}
+		else{
+			$activeBox = "";
+		}
+		##de-encrypt password
+		$pass = $row["password"];
+	}catch(PDOException $e){
+		echo "Connection failed: " . $e->getMessage();
+	}
+}
+?>
