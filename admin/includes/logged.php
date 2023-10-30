7f1ba19bd1ef682765e290bd27fb0b8324e05f6b
PerfectionistAF <ihabsohaila@yahoo.com>
2023-10-30 14:33:53 +0200

    All files removed from interfacev2

--- admin/includes/logged.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();#NEVER COMMENTED
-if(!isset($_SESSION["logged"]) or !$_SESSION["logged"]){   ##if not logged in  ##session is either not set or set to be false
-  header("Location: login.php");
-  die();
-}
-#session_destroy();
-?>
